d61085f05528007934884b2f5e2fdcc37004c72c
unknown <wonjae0709@naver.com>
2023-08-11 23:51:32 +0900

    feat: 바텀 탭 다시 변경

--- src/navigations/BottomStack.tsx
+++ src/navigations/BottomStack.tsx
@@ -7,10 +7,16 @@ import Posting from 'screens/posting/PostingMain';
 import Topic from 'screens/topic/Topic';
 import BoardStack from './BoardStack';
 import MyPage from 'screens/myPage';
-import BottomTabBar from 'components/BottomTabBar';
+import {
+  Feather,
+  MaterialCommunityIcons,
+  Octicons,
+} from 'utils/react-native-vector-helper';
 import {useAppSelector} from 'store/hooks';
 import {RootState} from 'store/store';
 import {BottomStackParamList} from './types';
+import {ss} from 'utils/scailing';
+import {black, gray} from 'styles';
 
 function BottomStack() {
   const Tab = createBottomTabNavigator<BottomStackParamList>();
@@ -18,38 +24,69 @@ function BottomStack() {
 
   return (
     <Tab.Navigator
-      tabBar={props => <BottomTabBar {...props} />}
-      backBehavior="initialRoute">
+      backBehavior="initialRoute"
+      initialRouteName="BoardStack"
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: black.origin,
+        tabBarInactiveTintColor: gray.origin,
+        tabBarStyle: {backgroundColor: gray.lightGray},
+        tabBarActiveBackgroundColor: gray.gainsboro,
+      }}>
       <Tab.Screen
         name="BoardStack"
         component={BoardStack}
         options={{
-          headerShown: false,
           tabBarLabel: '홈',
+          tabBarIcon: ({focused}) => (
+            <Feather
+              name="home"
+              color={focused ? black.origin : gray.origin}
+              size={ss(20)}
+            />
+          ),
         }}
       />
       <Tab.Screen
         name="Topic"
         component={Topic}
         options={{
-          headerShown: false,
           tabBarLabel: '주제별게시판',
+          tabBarIcon: ({focused}) => (
+            <MaterialCommunityIcons
+              name="clipboard-list-outline"
+              color={focused ? black.origin : gray.origin}
+              size={ss(20)}
+            />
+          ),
         }}
       />
       <Tab.Screen
         name="Posting"
         component={userId ? Posting : Login}
         options={{
-          headerShown: false,
           tabBarLabel: '글작성',
+          tabBarIcon: ({focused}) => (
+            <MaterialCommunityIcons
+              name="pencil-outline"
+              color={focused ? black.origin : gray.origin}
+              size={ss(20)}
+            />
+          ),
         }}
       />
       <Tab.Screen
         name="MyPage"
         component={userId ? MyPage : Login}
         options={{
-          headerShown: false,
           tabBarLabel: '마이페이지',
+          tabBarIcon: ({focused}) => (
+            <Octicons
+              name="person"
+              color={focused ? black.origin : gray.origin}
+              size={ss(20)}
+            />
+          ),
         }}
       />
     </Tab.Navigator>
--- src/styles/index.js
+++ src/styles/index.js
@@ -2,6 +2,7 @@ export const gray = {
   lightGray: '#e9e9e9',
   dimGray: '#696969',
   gainsboro: '#D0D0D0',
+  origin: '#808080',
 };
 
 export const white = {
