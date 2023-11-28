d1079b3220ae938fd04eb871e032727f9815c940
unknown <wonjae0709@naver.com>
2023-11-29 00:17:58 +0900

    Feat: 앱 알림설정 허락받기 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React, {useEffect} from 'react';
 import {Platform, View, Text, StatusBar} from 'react-native';
+import {request, PERMISSIONS, RESULTS, check} from 'react-native-permissions';
+
+import messaging from '@react-native-firebase/messaging';
 // import codePush from 'react-native-code-push';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import SplashScreen from 'react-native-splash-screen';
@@ -11,8 +14,57 @@ import {store} from 'store/store';
 
 const App = () => {
   useEffect(() => {
-    SplashScreen.hide();
+    const initializeApp = async () => {
+      SplashScreen.hide();
+      await requestPermission();
+    };
+    initializeApp();
   }, []);
+
+  const requestPermission = async () => {
+    const checkPermission = await checkNotificationPermission();
+    if (checkPermission !== RESULTS.GRANTED) {
+      const request = await requestNotificationPermission();
+      if (request !== RESULTS.GRANTED) {
+        console.log(request);
+        // permission not granted
+      }
+    }
+  };
+
+  const requestNotificationPermission = async () => {
+    const result = await request(PERMISSIONS.ANDROID.POST_NOTIFICATIONS);
+    return result;
+  };
+
+  const checkNotificationPermission = async () => {
+    const result = await check(PERMISSIONS.ANDROID.POST_NOTIFICATIONS);
+    return result;
+  };
+
+  // useEffect(() => {
+  //   // Assume a message-notification contains a "type" property in the data payload of the screen to open
+
+  //   messaging().onNotificationOpenedApp(remoteMessage => {
+  //     console.log(
+  //       'Notification caused app to open from background state:',
+  //       remoteMessage.notification,
+  //     );
+  //   });
+
+  //   // Check whether an initial notification is available
+  //   messaging()
+  //     .getInitialNotification()
+  //     .then(remoteMessage => {
+  //       if (remoteMessage) {
+  //         console.log(
+  //           'Notification caused app to open from quit state:',
+  //           remoteMessage.notification,
+  //         );
+  //         setInitialRoute(remoteMessage.data.type); // e.g. "Settings"
+  //       }
+  //     });
+  // }, []);
 
   if ((Platform.Version as number) < MIN_ANDROID_VERSION) {
     return (
