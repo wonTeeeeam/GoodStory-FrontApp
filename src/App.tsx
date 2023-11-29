f2b227b122ab1bb4ceff32d2c858ef1d960a20b6
unknown <wonjae0709@naver.com>
2023-11-29 22:45:05 +0900

    feat: FCM 토큰 알람 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {Platform, View, Text, StatusBar} from 'react-native';
+import {Platform, View, Text, StatusBar, Alert} from 'react-native';
 import {request, PERMISSIONS, RESULTS, check} from 'react-native-permissions';
 
 import messaging from '@react-native-firebase/messaging';
@@ -42,29 +42,60 @@ const App = () => {
     return result;
   };
 
-  // useEffect(() => {
-  //   // Assume a message-notification contains a "type" property in the data payload of the screen to open
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
 
-  //   messaging().onNotificationOpenedApp(remoteMessage => {
-  //     console.log(
-  //       'Notification caused app to open from background state:',
-  //       remoteMessage.notification,
-  //     );
-  //   });
+    return unsubscribe;
+  }, []);
 
-  //   // Check whether an initial notification is available
-  //   messaging()
-  //     .getInitialNotification()
-  //     .then(remoteMessage => {
-  //       if (remoteMessage) {
-  //         console.log(
-  //           'Notification caused app to open from quit state:',
-  //           remoteMessage.notification,
-  //         );
-  //         setInitialRoute(remoteMessage.data.type); // e.g. "Settings"
-  //       }
-  //     });
-  // }, []);
+  // 토큰 설정
+  useEffect(() => {
+    async function getToken() {
+      try {
+        // 기기 등록이 안되어있다면, 기기 토큰을 먼저 등록해야 한다.
+        if (!messaging().isDeviceRegisteredForRemoteMessages) {
+          await messaging().registerDeviceForRemoteMessages();
+        }
+
+        // 토큰 가져오기
+        const token = await messaging().getToken();
+        console.log('phone token', token);
+
+        // 유저 토큰 저장하기
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getToken();
+  }, []);
+
+  useEffect(() => {
+    // Assume a message-notification contains a "type" property in the data payload of the screen to open
+
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log(
+        'Notification caused app to open from background state:',
+        remoteMessage.notification,
+      );
+      // navigation.navigate(remoteMessage?.data?.type);
+    });
+
+    // Check whether an initial notification is available
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          );
+          // setInitialRoute(remoteMessage?.data?.type); // e.g. "Settings"
+        }
+      });
+  }, []);
 
   if ((Platform.Version as number) < MIN_ANDROID_VERSION) {
     return (
